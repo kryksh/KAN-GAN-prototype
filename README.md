361ad555d9347186d3e56d1a647bdb1ef926b2ef
Gleb Glukhov <glebgluhov@MacBook-Air-Gleb.local>
2025-05-07 04:45:05 +0300

    Add models and exps

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # KAN-GAN-prototype
-KAN-GAN prototype
+
+The repository contains modifications of methods for generating synthetic tabular data using KAN instead of MLP.
+
+The models folder contains modifications of TVAE and CTGAN models from [here](https://github.com/sdv-dev/CTGAN/tree/main/ctgan/synthesizers), where instead of Linear-layers is used KANLinear-layers from [here](https://github.com/Blealtan/efficient-kan).
+
+The notebook experiment contains an example of data generation using different models, as well as proposed modifications. Experimental studies contain two parts: (1) solving the classification and multi-label classification problem on different synthetic data and comparing metrics; (2) comparing synthetic and real data according to metrics from the [SDMetrics library](https://github.com/sdv-dev/SDMetrics).
---
+++ models/kan_tvae.py
@@ -0,0 +1,199 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.nn import Linear, Module, Parameter, ReLU, Sequential, SiLU
+from torch.nn.functional import cross_entropy
+from torch.optim import Adam
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+
+from ctgan.data_transformer import DataTransformer
+from ctgan.synthesizers.base import BaseSynthesizer, random_state
+
+from efficient_kan import KAN, KANLinear
+
+class Encoder(Module):
+    def __init__(self, data_dim, compress_dims, embedding_dim):
+        super(Encoder, self).__init__()
+        dim = data_dim
+        seq = []
+        for item in list(compress_dims):
+            seq += [KANLinear(dim, item), ReLU()]
+            dim = item
+
+        self.seq = Sequential(*seq)
+        self.fc1 = KANLinear(dim, embedding_dim)
+        self.fc2 = KANLinear(dim, embedding_dim)
+
+    def forward(self, input_):
+        feature = self.seq(input_)
+        mu = self.fc1(feature)
+        logvar = self.fc2(feature)
+        std = torch.exp(0.5 * logvar)
+        return mu, std, logvar
+
+
+class Decoder(Module):
+    def __init__(self, embedding_dim, decompress_dims, data_dim):
+        super(Decoder, self).__init__()
+        dim = embedding_dim
+        seq = []
+        for item in list(decompress_dims):
+            seq += [KANLinear(dim, item), ReLU()]
+            dim = item
+
+        seq.append(KANLinear(dim, data_dim))
+        self.seq = Sequential(*seq)
+        self.sigma = Parameter(torch.ones(data_dim) * 0.1)
+
+    def forward(self, input_):
+        return self.seq(input_), self.sigma
+
+
+def _loss_function(recon_x, x, sigmas, mu, logvar, output_info, factor):
+    st = 0
+    loss = []
+    for column_info in output_info:
+        for span_info in column_info:
+            if span_info.activation_fn != 'softmax':
+                ed = st + span_info.dim
+                std = sigmas[st]
+                eq = x[:, st] - torch.tanh(recon_x[:, st])
+                loss.append((eq**2 / 2 / (std**2)).sum())
+                loss.append(torch.log(std) * x.size()[0])
+                st = ed
+
+            else:
+                ed = st + span_info.dim
+                loss.append(
+                    cross_entropy(
+                        recon_x[:, st:ed], torch.argmax(x[:, st:ed], dim=-1), reduction='sum'
+                    )
+                )
+                st = ed
+
+    assert st == recon_x.size()[1]
+    KLD = -0.5 * torch.sum(1 + logvar - mu**2 - logvar.exp())
+    return sum(loss) * factor / x.size()[0], KLD / x.size()[0]
+
+
+class KANTVAE(BaseSynthesizer):
+
+    def __init__(
+        self,
+        embedding_dim=128,
+        compress_dims=(128, 128),
+        decompress_dims=(128, 128),
+        l2scale=1e-5,
+        batch_size=500,
+        epochs=300,
+        loss_factor=2,
+        cuda=True,
+        verbose=False,
+    ):
+        self.embedding_dim = embedding_dim
+        self.compress_dims = compress_dims
+        self.decompress_dims = decompress_dims
+
+        self.l2scale = l2scale
+        self.batch_size = batch_size
+        self.loss_factor = loss_factor
+        self.epochs = epochs
+        self.loss_values = pd.DataFrame(columns=['Epoch', 'Batch', 'Loss'])
+        self.verbose = verbose
+
+        if not cuda or not torch.cuda.is_available():
+            device = 'cpu'
+        elif isinstance(cuda, str):
+            device = cuda
+        else:
+            device = 'cuda'
+
+        self._device = torch.device(device)
+
+    @random_state
+    def fit(self, train_data, discrete_columns=()):
+        self.transformer = DataTransformer()
+        self.transformer.fit(train_data, discrete_columns)
+        train_data = self.transformer.transform(train_data)
+        dataset = TensorDataset(torch.from_numpy(train_data.astype('float32')).to(self._device))
+        loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True, drop_last=False)
+
+        data_dim = self.transformer.output_dimensions
+        encoder = Encoder(data_dim, self.compress_dims, self.embedding_dim).to(self._device)
+        self.decoder = Decoder(self.embedding_dim, self.decompress_dims, data_dim).to(self._device)
+        optimizerAE = Adam(
+            list(encoder.parameters()) + list(self.decoder.parameters()), weight_decay=self.l2scale
+        )
+
+        self.loss_values = pd.DataFrame(columns=['Epoch', 'Batch', 'Loss'])
+        iterator = tqdm(range(self.epochs), disable=(not self.verbose))
+        if self.verbose:
+            iterator_description = 'Loss: {loss:.3f}'
+            iterator.set_description(iterator_description.format(loss=0))
+
+        for i in iterator:
+            loss_values = []
+            batch = []
+            for id_, data in enumerate(loader):
+                optimizerAE.zero_grad()
+                real = data[0].to(self._device)
+                mu, std, logvar = encoder(real)
+                eps = torch.randn_like(std)
+                emb = eps * std + mu
+                rec, sigmas = self.decoder(emb)
+                loss_1, loss_2 = _loss_function(
+                    rec,
+                    real,
+                    sigmas,
+                    mu,
+                    logvar,
+                    self.transformer.output_info_list,
+                    self.loss_factor,
+                )
+                loss = loss_1 + loss_2
+                loss.backward()
+                optimizerAE.step()
+                self.decoder.sigma.data.clamp_(0.01, 1.0)
+
+                batch.append(id_)
+                loss_values.append(loss.detach().cpu().item())
+
+            epoch_loss_df = pd.DataFrame({
+                'Epoch': [i] * len(batch),
+                'Batch': batch,
+                'Loss': loss_values,
+            })
+            if not self.loss_values.empty:
+                self.loss_values = pd.concat([self.loss_values, epoch_loss_df]).reset_index(
+                    drop=True
+                )
+            else:
+                self.loss_values = epoch_loss_df
+
+            if self.verbose:
+                iterator.set_description(
+                    iterator_description.format(loss=loss.detach().cpu().item())
+                )
+
+    @random_state
+    def sample(self, samples):
+        self.decoder.eval()
+
+        steps = samples // self.batch_size + 1
+        data = []
+        for _ in range(steps):
+            mean = torch.zeros(self.batch_size, self.embedding_dim)
+            std = mean + 1
+            noise = torch.normal(mean=mean, std=std).to(self._device)
+            fake, sigmas = self.decoder(noise)
+            fake = torch.tanh(fake)
+            data.append(fake.detach().cpu().numpy())
+
+        data = np.concatenate(data, axis=0)
+        data = data[:samples]
+        return self.transformer.inverse_transform(data, sigmas.detach().cpu().numpy())
+
+    def set_device(self, device):
+        self._device = device
+        self.decoder.to(self._device)
